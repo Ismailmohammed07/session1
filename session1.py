d2d3c5fa23c02af87dde7a25eabd69a549bfae9b
Ismailmohammed07 <233555@students.aih.nsw.edu.au>
2024-05-27 18:39:46 +1000

    Create session1.py

---
+++ session1.py
@@ -0,0 +1,11 @@
+# write a program to calculate the GST tax of two products apple and pie
+# apple has 10% GST and pie has only 8%
+# calculate total GST that need to be deducted from the bill
+# orange gross sale is $75,pie gross is 100$
+
+apple = 75
+pie = 100
+GST_apple = 0.1*apple
+GST_pie = 0.08*pie
+print ["GST_apple"]
+print ["GST_pie"]
